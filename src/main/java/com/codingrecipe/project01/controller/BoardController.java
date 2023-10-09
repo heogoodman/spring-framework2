7a064fae923b472aaa23f6c67d338afec6113fe0
heogoodman <tjdwo1030@naver.com>
2023-10-09 16:13:52 +0900

    second upload

---
+++ src/main/java/com/codingrecipe/project01/controller/BoardController.java
@@ -0,0 +1,71 @@
+package com.codingrecipe.project01.controller;
+
+import com.codingrecipe.project01.dto.BoardDTO;
+import com.codingrecipe.project01.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        int saveResult = boardService.save(boardDTO);
+        if(saveResult > 0) {
+            return "redirect:/board/";
+        } else {
+            return "save";
+        }
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList",boardDTOList);
+        return "list";
+    }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model){
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board",boardDTO);
+        return "detail";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(@RequestParam("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board",boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO,Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findById(boardDTO.getId());
+        model.addAttribute("board", dto);
+        return "detail";
+//        return "redirect:/board?id=" + boardDTO.getId();
+    }
+
+}
---
+++ src/main/java/com/codingrecipe/project01/service/BoardService.java
@@ -0,0 +1,38 @@
+package com.codingrecipe.project01.service;
+
+import com.codingrecipe.project01.dto.BoardDTO;
+import com.codingrecipe.project01.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    public int save(BoardDTO boardDTO) {
+        return boardRepository.save(boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+    }
+
+    public BoardDTO findById(Long id) {
+        return boardRepository.findById(id);
+    }
+
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public void delete(Long id) {
+        boardRepository.delete(id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        boardRepository.update(boardDTO);
+    }
+}
